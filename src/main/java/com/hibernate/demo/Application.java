2ee50cf04e03bb0dd5fef0f6df3927a7e7769b12
Pulkit Pushkarna <pulkitpushkarna@Pulkits-MacBook-Pro.local>
2017-01-20 19:39:43 +0530

    Person object persisted

--- src/main/java/com/hibernate/demo/Application.java
+++ src/main/java/com/hibernate/demo/Application.java
@@ -1,7 +1,20 @@
 package com.hibernate.demo;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Application {
     public static void main(String[] args) {
-
+        Person person=new Person();
+        person.setName("Hibernate");
+        person.setAge(23);
+        person.setId(1);
+        SessionFactory sessionFactory=new Configuration().configure().buildSessionFactory();
+        Session session=sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(person);
+        session.getTransaction().commit();
+        session.close();
     }
 }
---
+++ src/main/java/com/hibernate/demo/Person.java
@@ -0,0 +1,45 @@
+package com.hibernate.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Person{
+    @Id
+    Integer id;
+    String name;
+    Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
